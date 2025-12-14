003b8be4d5c0ad605dc879858521c3d54d476393
moonsunchoi0609 <moonsunchoi0609@gmail.com>
2025-12-14 15:45:46 +0900

    Add idb dependency

---
+++ android/app/src/main/java/com/myiep/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myiep.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myiep.app',
+  appName: 'My-IEP-App',
+  webDir: 'dist'
+};
+
+export default config;
