e3d5a13d80e6ecdf138af99d57c337a1afee6764
moonsunchoi0609 <moonsunchoi0609@gmail.com>
2025-12-20 11:57:18 +0900

    Fix the PWA icon path and complete Tailwind settings.

---
+++ capacitor.config.ts
@@ -0,0 +1,23 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myiep.app',
+  appName: 'My IEP App',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: [
+        "profile",
+        "email",
+        "https://www.googleapis.com/auth/drive.file"
+      ],
+      serverClientId: "910593096810-ta1dn24769q4loaqjes10e5gkh2gfhml.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true
+    }
+  }
+  // ▲▲▲ 여기까지 ▲▲▲
+};
+export default config;
